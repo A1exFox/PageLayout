0cc02461703c2f75848625ec33a64e41658191ea
A1exFox <oxoma91@gmail.com>
2023-05-31 16:02:45 +0300

    feat: correct burger menu

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-const isOpenedMenu = className => Boolean(document.body.classList.contains(className));
+const isOpenedMenu = className => !!document.body.classList.contains(className);
 const toggleMenu = className => document.body.classList.toggle(className);
 
 document.addEventListener('click', clickSomewhere);
 function clickSomewhere(event) {
   if (isOpenedMenu('showMobileMenu')) {
-    if (event.target.classList.contains('headerMenu')) return;
-    if (event.target.classList.contains('headerMenu__list')) return;
+    if (isIgnoreElements(event.target)) return;
     toggleMenu('showMobileMenu');
   } else if (event.target.closest('.burger')) {
     toggleMenu('showMobileMenu');
   }
+}
+function isIgnoreElements(target) {
+  if (target.classList.contains('headerMenu')) return true;
+  if (target.classList.contains('headerMenu__list')) return true;
+  return false;
 }
